d8065cb50c87ba14f6a0a3e1f3b855d22bd86be5
Timofey Serov <115070403+Dedule@users.noreply.github.com>
2023-04-13 09:53:39 +0300

    my res

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,10 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld helloWorld2 = (HelloWorld) applicationContext.getBean("helloworld");
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(bean == helloWorld2);
+        System.out.println(cat1 == cat2);
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,5 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+public class Cat {
+}
